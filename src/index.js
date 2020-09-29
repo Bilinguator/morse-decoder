165adb3e81da2c06445b89ed8400a56ca2ce8a24
Dmitrii Snitkin <snitkin.d@list.ru>
2020-09-29 20:09:23 +0300

    feat: implement morse-decoder function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    //Деление массива на строки по 10 символов
+    let matrix = [];
+    for (i = 0; i < expr.length; i += 10) {
+        matrix.push(expr.slice(i, i + 10));
+    }
+    
+    //Удаление нулей слева каждой строки и перевод на язык Морзе
+    let index = 0;
+    for (i = 0; i < matrix.length; i++) {
+        for (j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === '0') {
+                index++;
+            } else {
+                matrix[i] = matrix[i].slice(index, matrix[i].length).split('10').join('.').split('11').join('-');
+                index = 0;
+            }
+        }
+    }
+
+    //Сборка результирующей строки
+    let result = '';
+    for (i = 0; i < matrix.length; i++) {
+        if (matrix[i] === '**********')
+            result += ' ';
+        else {
+            result += MORSE_TABLE[matrix[i]];
+        }
+    }
+
+    return result;
+
 }
 
 module.exports = {
